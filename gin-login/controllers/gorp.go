f9b577d0740e2121de2ca26e1ab473cc9cff9471
Basel Rabia <baselrabia@gmail.com>
2021-09-30 18:10:58 +0200

    create the user table
    it throws an error if I create a user in the post request that go cant see that the table is created before, so I figure the gorp package and in there example, they create a table before checking if tables exist

--- gin-login/controllers/gorp.go
+++ gin-login/controllers/gorp.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"database/sql"
 	"log"
+	"goapi/models"
+
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-gorp/gorp"
@@ -15,6 +17,13 @@ func initDb() *gorp.DbMap {
 	db, err := sql.Open("mysql", "root:Bumitama2016!mysql@tcp(localhost:3306)/test")
 	checkErr(err, "sql.Open failed")
 	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
+	
+	// add a table, setting the table name to 'user' and
+    	// specifying that the Id property is an auto incrementing PK
+ 	dbmap.AddTableWithName(models.User{}, "user").SetKeys(true, "Id")
+
+	// create the table. in a production system you'd generally
+	// use a migration tool, or create the tables via scripts
 	err = dbmap.CreateTablesIfNotExists()
 	checkErr(err, "Create tables failed")
 
